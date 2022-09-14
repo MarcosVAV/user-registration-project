b5c5153ab25ec7225b47e9a283c823a79b8ae1ae
Marcos Veloso <marcosvictor12345@gmail.com>
2022-09-13 21:16:05 -0300

    add crud address

--- database/migrations/2022_09_13_235930_create_addresses_table.php
+++ database/migrations/2022_09_13_235930_create_addresses_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->string('address');
+            $table->integer('number');
+            $table->string('district');
+            $table->string('cep');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('addresses');
     }
 };
